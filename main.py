8eeebb8e9ccf4c4bdac52b809fdd3292e619fdb4
DaielChom <danielcarvajalpatino@gmail.com>
2017-06-30 11:59:40 -0500

    GETs main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from config import DevelopmentConfig
 from flask_wtf import CSRFProtect
 from modelo import db
 from flask import render_template
+import formpy
 
 # init ...
 app = Flask(__name__) # Flask
@@ -18,17 +19,29 @@ def index():
     #print(db.get_tables_for_bind())
     return render_template('index.html', title = "QRTraker", tables = tables)
 
+
 # Route funcionarios
 @app.route('/funcionarios', methods=['GET', 'POST'])
 def funcionarios():
+
+    # Intancia de un formulario para Funcionarios
+    funcionario_form = formpy.Official(None)
+
+    # GET
     if request.method == 'GET':
-        return render_template('funcionarios.html', title = "funcionarios", tables = tables)
+        return render_template('funcionarios.html', title = "funcionarios", tables = tables, funcionario_form = funcionario_form)
+
 
 # Route clientes
 @app.route('/clientes', methods=['GET', 'POST'])
 def cientes():
+
+    # Instance of client form
+    client_form = formpy.Client(None)
+
+    # GET
     if request.method == 'GET':
-        return render_template('clientes.html', title = "clientes", tables = tables)
+        return render_template('clientes.html', title = "clientes", tables = tables, client_form = client_form )
 
 # Route paquetes
 @app.route('/paquetes', methods=['GET', 'POST'])
