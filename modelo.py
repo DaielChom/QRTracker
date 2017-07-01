cf04a4c39d61372b8bfc2153d75d5502aa330eeb
DaielChom <danielcarvajalpatino@gmail.com>
2017-06-30 23:59:46 -0500

    Modelo

--- modelo.py
+++ modelo.py
@@ -9,9 +9,9 @@ class Official(db.Model):
     __tablename__ = "Official"
 
     # Atributes
-    id_offcial = db.Column(db.String(20), primary_key = True, nullable = False)
+    id_official = db.Column(db.String(20), primary_key = True, nullable = False)
     nanme_official = db.Column(db.String(20), nullable = True)
-    packages = db.relationship("Package", backref="client", lazy ="dinamic")
+    packages = db.relationship("Package", backref="Official", lazy ="dinamic")
 
 
     # Constructor
@@ -26,26 +26,26 @@ class Client(db.Model):
     id_client = db.Column(db.String(20), primary_key= True, nullable = False)
     name_client = db.Column(db.String(20), nullable= False)
     address_client = db.Column(db.String(20), nullable= False)
-    packages = db.relationship("Package", backref="client", lazy ="dinamic")
+    packages = db.relationship("Package", backref="Client", lazy ="dinamic")
 
     def __init__(self, id_client, name_client, address_client):
         self.id_client = id_client
         self.name_client = name_client
         self.address_client = address_client
 
-class Package(db.Model)
+class Package(db.Model):
 
-    _talbename_ = "Package"
+    __tablename__ = "Package"
 
     id_package = db.Column(db.String(20), primary_key = True, nullable = False)
-    client = db.Column(db.String(20), db.ForeignKey('client.id_client'))
+    client = db.Column(db.String(20), db.ForeignKey('Client.id_client'))
     descrption_package = db.Column(db.Text(), nullable = False)
     estate_package = db.Column(db.String(20), nullable = False)
 
 monitor = db.Table('monitor',
                     db.Column('id_monitor', db.String(20), primary_key = True, nullable = True),
-                    db.Column('package', db.String(20), db.ForeignKey('package.id_package')),
-                    db.Column('Official', db.String(20), db.ForeignKey('official.id_official')),
+                    db.Column('package', db.String(20), db.ForeignKey('Package.id_package')),
+                    db.Column('official', db.String(20), db.ForeignKey('Official.id_official')),
                     db.Column('long_monitor', db.Float(), nullable = False),
                     db.Column('lati_monitor', db.Float(), nullable = False),
                     db.Column('alti_monitor', db.Integer(), nullable = False),
